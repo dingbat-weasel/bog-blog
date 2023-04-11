7d77ce2dec0142c63a2c07459f6879992ccca13d
Isaac Perk <97003977+isaacperk@users.noreply.github.com>
2023-04-10 21:40:17 -0700

    included login routes and login logic in public/js

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const { Article, User } = require("../models");
+const withAuth = require("../utils/auth");
 
 router.get("/", async (req, res) => {
   try {
@@ -19,28 +20,60 @@ router.get("/", async (req, res) => {
     // Pass into template
     res.render("homepage", {
       articles,
+      logged_in: req.session.logged_in,
     });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get("/users", async (req, res) => {
+router.get("/article/:id", async (req, res) => {
   try {
-    // Get all users, sorted by name
-    const userData = await User.findAll({
-      attributes: { exclude: ["password"] },
-      order: [["name", "ASC"]],
+    const articleData = await Article.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ["name"],
+        },
+      ],
     });
 
-    // Serialize user data so templates can read it
-    const users = userData.map((project) => project.get({ plain: true }));
+    const article = articleData.get({ plain: true });
 
-    // Pass serialized data into Handlebars.js template
-    res.render("users", { users });
+    res.render("article", {
+      ...article,
+      logged_in: req.session.logged_in,
+    });
   } catch (err) {
     res.status(500).json(err);
   }
 });
+
+router.get("/dashboard", withAuth, async (req, res) => {
+  try {
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ["password"] },
+      include: [{ model: Article }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render("dashboard", {
+      ...user,
+      logged_in: true,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/login', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/dashboard');
+    return;
+  }
+
+  res.render('login');
+})
 
 module.exports = router;
