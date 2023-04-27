968b12d276652212d82a362f7fc713ed501595fe
Isaac Perk <97003977+isaacperk@users.noreply.github.com>
2023-04-27 11:56:33 -0700

    added prettier/eslint stuff and cleaned up forms

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -2,15 +2,12 @@ const newFormHandler = async (event) => {
   event.preventDefault();
 
   const name = document.querySelector('#article-name').value.trim();
-  const needed_funding = document
-    .querySelector('#article-funding')
-    .value.trim();
   const description = document.querySelector('#article-desc').value.trim();
 
-  if (name && needed_funding && description) {
+  if (name && description) {
     const response = await fetch(`/api/articles`, {
       method: 'POST',
-      body: JSON.stringify({ name, needed_funding, description }),
+      body: JSON.stringify({ name, description }),
       headers: {
         'Content-Type': 'application/json',
       },
